131fb58633af8266d06dfbe5802c65dd7cd26fdd
vladimir <vladimi.sedlar@monsoonconsulting.com>
2024-02-16 22:29:23 +0100

    app: add consume events command

---
+++ apps/targetadds/front/src/Command/DomainEvents/MySql/ConsumeMySqlEvents.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\Apps\TargetAdds\Front\Command\DomainEvents\MySql;
+
+use Acme\Shared\Domain\Bus\Event\DomainEvent;
+
+use CodelyTv\Shared\Infrastructure\Bus\Event\DomainEventSubscriberLocator;
+use CodelyTv\Shared\Infrastructure\Bus\Event\MySql\MySqlDoctrineDomainEventsConsumer;
+
+use Acme\Shared\Infrastructure\Doctrine\DatabaseConnections;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use function Lambdish\Phunctional\pipe;
+
+#[AsCommand(name: 'acme:domain-events:mysql:consume', description: 'Consume domain events from MySql',)]
+final class ConsumeMySqlEvents extends Command
+{
+  public function __construct(
+    private readonly MySqlDoctrineDomainEventsConsumer $consumer,
+    private readonly DatabaseConnections $connections,
+    private readonly DomainEventSubscriberLocator $subscriberLocator
+  ) {
+    parent::__construct();
+  }
+
+  protected function configure(): void
+  {
+    $this->addArgument('quantity', InputArgument::REQUIRED, 'Quantity of events to process');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output): int
+  {
+    $quantityEventsToProcess = (int) $input->getArgument('quantity');
+
+    $consumer = pipe($this->consumer(), fn () => $this->connections->clear());
+
+    $this->consumer->consume($consumer, $quantityEventsToProcess);
+
+    return 0;
+  }
+
+  private function consumer(): callable
+  {
+    return function (DomainEvent $domainEvent): void {
+      $subscribers = $this->subscriberLocator->allSubscribedTo($domainEvent::class);
+
+      foreach ($subscribers as $subscriber) {
+        $subscriber($domainEvent);
+      }
+    };
+  }
+}

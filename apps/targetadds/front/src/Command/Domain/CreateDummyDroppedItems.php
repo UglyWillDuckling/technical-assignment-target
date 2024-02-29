c827625b4c6b45f0232c237c7d2ce1112ba30c05
vladimir <vladimi.sedlar@monsoonconsulting.com>
2024-02-29 23:02:49 +0100

    chore(app,command): cleanup commands

--- apps/targetadds/front/src/Command/Domain/CreateDummyDroppedItems.php
+++ apps/targetadds/front/src/Command/Domain/CreateDummyDroppedItems.php
@@ -1,10 +1,11 @@
-<?php
+<?php /** @noinspection ALL */
 
 declare(strict_types=1);
 
 namespace Acme\Apps\TargetAdds\Front\Command\Domain;
 
 use Acme\TargetAdds\Tracking\Domain\DroppedItem;
+use Acme\TargetAdds\Tracking\Domain\DroppedItemsCollection;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -16,26 +17,28 @@ use Faker\Factory;
 #[AsCommand(name: 'acme:domain-dummy-create_dropped_items', description: 'Create Dummy Dropped Items',)]
 final class CreateDummyDroppedItems extends Command
 {
-  public function __construct(private readonly DroppedItemRepository $droppedItemRepository) {
-    parent::__construct();
-  }
+    public function __construct(private readonly DroppedItemRepository $droppedItemRepository)
+    {
+        parent::__construct();
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
-  {
-      // TODO: We can make this an Argument
-      $howMuch = 1000;
+    {
+        $howMuch = 100000;
+        $faker = Factory::create();
 
-      $faker = Factory::create();
+        $items = [];
+        for ($i = 0; $i <= $howMuch; $i++) {
+            $customer_id = (string)random_int(1, 1000);
+            $sku = $faker->word();
+            $id = Uuid::uuid4()->toString();
 
-      for ($i = 0; $i <= $howMuch; $i++) {
-          $customer_id = (string)random_int(1, 1000);
-          $sku = $faker->word();
-          $id = Uuid::uuid4()->toString();
+            $items[] = new DroppedItem($id, $customer_id, $sku);
+        }
+        $coll = new DroppedItemsCollection($items, 1);
 
-           $item = new DroppedItem($id, $customer_id, $sku);
-           $this->droppedItemRepository->save($item);
-      }
+        $this->droppedItemRepository->saveCollection($coll);
 
-      return Command::SUCCESS;
-  }
+        return Command::SUCCESS;
+    }
 }
--- apps/targetadds/front/src/Command/Domain/GetDroppedItemsByProduct.php
+++ apps/targetadds/front/src/Command/Domain/GetDroppedItemsByProduct.php
@@ -8,16 +8,17 @@ use Acme\Shared\Domain\Criteria\Criteria;
 use Acme\Shared\Domain\Criteria\Filters;
 use Acme\Shared\Domain\Criteria\Order;
 use Acme\Shared\Domain\Utils;
+use Acme\TargetAdds\Tracking\Domain\DroppedItem\DroppedItemsByProductQuery;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Acme\TargetAdds\Tracking\Domain\DroppedItemRepository;
 
 #[AsCommand(name: 'acme:domain-get_dropped_byproduct', description: 'Get Items Grouped by product',)]
 final class GetDroppedItemsByProduct extends Command
 {
-  public function __construct(private readonly DroppedItemRepository $droppedItemRepository) {
+  public function __construct(
+      private readonly DroppedItemsByProductQuery $droppedItemsByProductQuery) {
     parent::__construct();
   }
 
@@ -27,25 +28,21 @@ final class GetDroppedItemsByProduct extends Command
         "operator": ">",
         "field": "created_at",
         "value": "2024-02-15"
-        }]';
+        },
+        {
+            "field": "total",
+            "operator": ">",
+            "value": "11"
+          }
+      ]';
 
-//      $filters = Filters::fromValues(Utils::jsonDecode($filterTxt));
-      $filters = Filters::fromValues([]);
-
+      $filters = Filters::fromValues(Utils::jsonDecode($filterTxt));
       $criteria = (new Criteria($filters, Order::none(),null,1));
 
-//      $criteria = Criteria::empty();
-
-//      die(var_dump($filters));
-      $collection = $this->droppedItemRepository->byProduct($criteria);
+      $collection = $this->droppedItemsByProductQuery->matching($criteria);
 
       $output->writeln((string)$collection->count());
       $output->writeln((string)$collection->countTotal());
-//      foreach ($result as $item) {
-//          echo "\n";
-//          echo $item->sku;
-//          echo $item->total;
-//      }
       return 0;
   }
 }
